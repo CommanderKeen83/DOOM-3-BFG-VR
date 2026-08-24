fc904cec10f5b3002009da10bdf45016a639aa1e
Commanderkeen83 <CommanderKeen83@protonmail.com>
2026-08-24 21:00:16 +0200

    Lower chaingun foregrip anchor point 15cm on Z axis

--- neo/d3xp/Weapon.h
+++ neo/d3xp/Weapon.h
@@ -101,11 +101,11 @@ const idVec3 foregripOffsets[int( WEAPON_NUM_WEAPONS )] = {	idVec3( 0.0f, 0.0f, 
 															idVec3( 0.0f, 0.0f, 0.0f ),			// WEAPON_PISTOL
 															idVec3( 15.0f, 0.0f, -1.0f ),		// WEAPON_SHOTGUN
 															idVec3( 14.5f, 0.0f, -1.5f ),		// WEAPON_MACHINEGUN
-															idVec3( 15.0f, -0.5f, 2.5f ),		// WEAPON_CHAINGUN
+															idVec3( 15.0f, 0.0f, -3.5f ),		// WEAPON_CHAINGUN
 															idVec3( 0.0f, 0.0f, 0.0f ),			// WEAPON_HANDGRENADE
 															idVec3( 14.5f, 0.0f, -1.5f ),		// WEAPON_PLASMAGUN
 															idVec3( 14.0f, 0.0f, -2.0f ),		// WEAPON_ROCKETLAUNCHER
-															idVec3( 14.0f, -0.5f, 2.0f ),		// WEAPON_BFG
+															idVec3( 14.0f, -0.5f, -2.0f ),		// WEAPON_BFG
 															idVec3( 0.0f, 0.0f, 0.0f ),			// WEAPON_SOULCUBE
 															idVec3( 13.5f, 0.0f, -1.5f ),		// WEAPON_SHOTGUN_DOUBLE
 															idVec3( 13.5f, 0.0f, -1.5f ),		// WEAPON_SHOTGUN_DOUBLE_MP
