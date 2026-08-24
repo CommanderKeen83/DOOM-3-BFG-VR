99e361f3056f847edacb18dbff940929e8262c61
Commanderkeen83 <CommanderKeen83@protonmail.com>
2026-08-24 20:51:48 +0200

    Fine-tune shotgun and two-handed foregrip positions forward

--- neo/d3xp/Weapon.h
+++ neo/d3xp/Weapon.h
@@ -99,17 +99,17 @@ const idVec3 foregripOffsets[int( WEAPON_NUM_WEAPONS )] = {	idVec3( 0.0f, 0.0f, 
 															idVec3( 0.0f, 0.0f, 0.0f ),			// WEAPON_FISTS
 															idVec3( 0.0f, 0.0f, 0.0f ),			// WEAPON_CHAINSAW
 															idVec3( 0.0f, 0.0f, 0.0f ),			// WEAPON_PISTOL
-															idVec3( 13.5f, 0.0f, -1.0f ),		// WEAPON_SHOTGUN
-															idVec3( 13.0f, 0.0f, -1.5f ),		// WEAPON_MACHINEGUN
+															idVec3( 15.0f, 0.0f, -1.0f ),		// WEAPON_SHOTGUN
+															idVec3( 14.5f, 0.0f, -1.5f ),		// WEAPON_MACHINEGUN
 															idVec3( 15.0f, -0.5f, 2.5f ),		// WEAPON_CHAINGUN
 															idVec3( 0.0f, 0.0f, 0.0f ),			// WEAPON_HANDGRENADE
-															idVec3( 13.0f, 0.0f, -1.5f ),		// WEAPON_PLASMAGUN
-															idVec3( 13.0f, 0.0f, -2.0f ),		// WEAPON_ROCKETLAUNCHER
-															idVec3( 13.0f, -0.5f, 2.0f ),		// WEAPON_BFG
+															idVec3( 14.5f, 0.0f, -1.5f ),		// WEAPON_PLASMAGUN
+															idVec3( 14.0f, 0.0f, -2.0f ),		// WEAPON_ROCKETLAUNCHER
+															idVec3( 14.0f, -0.5f, 2.0f ),		// WEAPON_BFG
 															idVec3( 0.0f, 0.0f, 0.0f ),			// WEAPON_SOULCUBE
-															idVec3( 12.0f, 0.0f, -1.5f ),		// WEAPON_SHOTGUN_DOUBLE
-															idVec3( 12.0f, 0.0f, -1.5f ),		// WEAPON_SHOTGUN_DOUBLE_MP
-															idVec3( 11.0f, 0.0f, -1.5f ),		// WEAPON_GRABBER
+															idVec3( 13.5f, 0.0f, -1.5f ),		// WEAPON_SHOTGUN_DOUBLE
+															idVec3( 13.5f, 0.0f, -1.5f ),		// WEAPON_SHOTGUN_DOUBLE_MP
+															idVec3( 12.5f, 0.0f, -1.5f ),		// WEAPON_GRABBER
 															idVec3( 0.0f, 0.0f, 0.0f ),			// WEAPON_ARTIFACT
 															idVec3( 0.0f, 0.0f, 0.0f )			// WEAPON_PDA
 };
