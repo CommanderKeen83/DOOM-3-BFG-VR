aef8f920862fa27f439198db17849d01b5ef1d0e
Commanderkeen83 <CommanderKeen83@protonmail.com>
2026-08-24 21:28:14 +0200

    Raise Chainsaw top handle and fine-tune Plasma Gun height

--- neo/d3xp/Weapon.h
+++ neo/d3xp/Weapon.h
@@ -97,13 +97,13 @@ const idVec3 flashOffsets[int( WEAPON_NUM_WEAPONS )] = {	idVec3( 0.0f, 0.0f, 0.0
 // Foregrip anchor offsets for two-handed weapons (x = forward along barrel, y = left/right, z = up/down)
 const idVec3 foregripOffsets[int( WEAPON_NUM_WEAPONS )] = {	idVec3( 0.0f, 0.0f, 0.0f ),			// WEAPON_NONE
 															idVec3( 0.0f, 0.0f, 0.0f ),			// WEAPON_FISTS
-															idVec3( 15.5f, -0.4f, 8.0f ),		// WEAPON_CHAINSAW
+															idVec3( 15.5f, -0.4f, 16.0f ),		// WEAPON_CHAINSAW
 															idVec3( 0.0f, 0.0f, 0.0f ),			// WEAPON_PISTOL
 															idVec3( 15.0f, -0.4f, -1.0f ),		// WEAPON_SHOTGUN
 															idVec3( 8.5f, -0.4f, 0.5f ),		// WEAPON_MACHINEGUN
 															idVec3( 7.0f, -0.4f, -3.5f ),		// WEAPON_CHAINGUN
 															idVec3( 0.0f, 0.0f, 0.0f ),			// WEAPON_HANDGRENADE
-															idVec3( 10.3f, -0.4f, -3.5f ),		// WEAPON_PLASMAGUN
+															idVec3( 10.3f, -0.4f, -3.1f ),		// WEAPON_PLASMAGUN
 															idVec3( 13.2f, -0.4f, 2.0f ),		// WEAPON_ROCKETLAUNCHER
 															idVec3( 14.0f, -0.5f, 0.8f ),		// WEAPON_BFG
 															idVec3( 0.0f, 0.0f, 0.0f ),			// WEAPON_SOULCUBE
