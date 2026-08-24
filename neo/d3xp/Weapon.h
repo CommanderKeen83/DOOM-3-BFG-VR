069c3c7844da06a8d7cfbf4666e52b437ff425c4
Commanderkeen83 <CommanderKeen83@protonmail.com>
2026-08-24 21:14:02 +0200

    Fine-tune lateral offset and plasmagun position in Weapon.h

--- neo/d3xp/Weapon.h
+++ neo/d3xp/Weapon.h
@@ -99,16 +99,16 @@ const idVec3 foregripOffsets[int( WEAPON_NUM_WEAPONS )] = {	idVec3( 0.0f, 0.0f, 
 															idVec3( 0.0f, 0.0f, 0.0f ),			// WEAPON_FISTS
 															idVec3( 0.0f, 0.0f, 0.0f ),			// WEAPON_CHAINSAW
 															idVec3( 0.0f, 0.0f, 0.0f ),			// WEAPON_PISTOL
-															idVec3( 15.0f, 0.0f, -1.0f ),		// WEAPON_SHOTGUN
-															idVec3( 8.5f, 0.0f, 0.5f ),			// WEAPON_MACHINEGUN
-															idVec3( 7.0f, 0.0f, -3.5f ),		// WEAPON_CHAINGUN
+															idVec3( 15.0f, -0.4f, -1.0f ),		// WEAPON_SHOTGUN
+															idVec3( 8.5f, -0.4f, 0.5f ),		// WEAPON_MACHINEGUN
+															idVec3( 7.0f, -0.4f, -3.5f ),		// WEAPON_CHAINGUN
 															idVec3( 0.0f, 0.0f, 0.0f ),			// WEAPON_HANDGRENADE
-															idVec3( 12.3f, 0.0f, -3.5f ),		// WEAPON_PLASMAGUN
+															idVec3( 11.5f, -0.4f, -3.5f ),		// WEAPON_PLASMAGUN
 															idVec3( 14.0f, 0.0f, -2.0f ),		// WEAPON_ROCKETLAUNCHER
 															idVec3( 14.0f, -0.5f, -2.0f ),		// WEAPON_BFG
 															idVec3( 0.0f, 0.0f, 0.0f ),			// WEAPON_SOULCUBE
-															idVec3( 13.5f, 0.0f, -1.5f ),		// WEAPON_SHOTGUN_DOUBLE
-															idVec3( 13.5f, 0.0f, -1.5f ),		// WEAPON_SHOTGUN_DOUBLE_MP
+															idVec3( 13.5f, -0.4f, -1.5f ),		// WEAPON_SHOTGUN_DOUBLE
+															idVec3( 13.5f, -0.4f, -1.5f ),		// WEAPON_SHOTGUN_DOUBLE_MP
 															idVec3( 12.5f, 0.0f, -1.5f ),		// WEAPON_GRABBER
 															idVec3( 0.0f, 0.0f, 0.0f ),			// WEAPON_ARTIFACT
 															idVec3( 0.0f, 0.0f, 0.0f )			// WEAPON_PDA
