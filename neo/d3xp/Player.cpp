ca2b3dac0c90742c4468ee94953ed59348f4ccd4
Commanderkeen83 <CommanderKeen83@protonmail.com>
2026-08-29 09:56:28 +0200

    Remove CVAR_ARCHIVE cache lock and set full index finger reach dimensions (5.5f, 1.5f, 0.6f)

--- neo/d3xp/Player.cpp
+++ neo/d3xp/Player.cpp
@@ -63,9 +63,9 @@ idCVar pm_clientAuthoritative_minSpeedSquared( "pm_clientAuthoritative_minSpeedS
 idCVar vr_wipScale( "vr_wipScale", "1.0", CVAR_FLOAT | CVAR_ARCHIVE, "" );
 
 idCVar vr_debugGui( "vr_debugGui", "0", CVAR_BOOL, "" );
-idCVar vr_offHandTouchForward( "vr_offHandTouchForward", "4.6", CVAR_FLOAT | CVAR_ARCHIVE, "Forward offset from wrist for off-hand fingertip touch ray" );
-idCVar vr_offHandTouchLateral( "vr_offHandTouchLateral", "0.75", CVAR_FLOAT | CVAR_ARCHIVE, "Lateral offset from wrist for off-hand fingertip touch ray (towards index finger)" );
-idCVar vr_offHandTouchVertical( "vr_offHandTouchVertical", "0.4", CVAR_FLOAT | CVAR_ARCHIVE, "Vertical offset from wrist for off-hand fingertip touch ray" );
+idCVar vr_offHandTouchForward( "vr_offHandTouchForward", "5.5", CVAR_FLOAT, "Forward offset from wrist for off-hand fingertip touch ray" );
+idCVar vr_offHandTouchLateral( "vr_offHandTouchLateral", "1.5", CVAR_FLOAT, "Lateral offset from wrist for off-hand fingertip touch ray (towards index finger)" );
+idCVar vr_offHandTouchVertical( "vr_offHandTouchVertical", "0.6", CVAR_FLOAT, "Vertical offset from wrist for off-hand fingertip touch ray" );
 idCVar vr_guiFocusPitchAdj( "vr_guiFocusPitchAdj", "7", CVAR_FLOAT | CVAR_ARCHIVE, "View pitch adjust to help activate in game Talk to NPC" );
 
 idCVar vr_bx1( "vr_bx1", "5", CVAR_FLOAT, "");
