d8449dcfb024c44e2c743b97b2ae5a06c0d0b1cc
Commanderkeen83 <CommanderKeen83@protonmail.com>
2026-08-29 09:59:10 +0200

    Extend off-hand forward touch offset from base of finger to fingertip (8.8 inches)

--- neo/d3xp/Player.cpp
+++ neo/d3xp/Player.cpp
@@ -63,7 +63,7 @@ idCVar pm_clientAuthoritative_minSpeedSquared( "pm_clientAuthoritative_minSpeedS
 idCVar vr_wipScale( "vr_wipScale", "1.0", CVAR_FLOAT | CVAR_ARCHIVE, "" );
 
 idCVar vr_debugGui( "vr_debugGui", "0", CVAR_BOOL, "" );
-idCVar vr_offHandTouchForward( "vr_offHandTouchForward", "5.5", CVAR_FLOAT, "Forward offset from wrist for off-hand fingertip touch ray" );
+idCVar vr_offHandTouchForward( "vr_offHandTouchForward", "8.8", CVAR_FLOAT, "Forward offset from wrist for off-hand fingertip touch ray" );
 idCVar vr_offHandTouchLateral( "vr_offHandTouchLateral", "1.5", CVAR_FLOAT, "Lateral offset from wrist for off-hand fingertip touch ray (towards index finger)" );
 idCVar vr_offHandTouchVertical( "vr_offHandTouchVertical", "0.6", CVAR_FLOAT, "Vertical offset from wrist for off-hand fingertip touch ray" );
 idCVar vr_guiFocusPitchAdj( "vr_guiFocusPitchAdj", "7", CVAR_FLOAT | CVAR_ARCHIVE, "View pitch adjust to help activate in game Talk to NPC" );
